ec9111229bc887a793aaa06d490ef71b7bb2913e
Luca Albergante <luca.albergante@gmail.com>
2017-02-14 23:28:14 +0100

    included weigth reading for GMT

--- R/GMT.R
+++ R/GMT.R
@@ -23,7 +23,21 @@ ReadGMTFile <- function(FileLocation) {
     if(length(tLine) < 3){
       Done <- TRUE
     } else {
-      GeneList[[Idx]] <- list(Name = tLine[1], Desc = tLine[2], Genes = tLine[-c(1:2)])
+      
+      tGenes <- tLine[-c(1:2)]
+      tGenes <- tGenes[!is.na(tGenes)]
+      tGenes <- strsplit(tGenes, split = "]", fixed = TRUE)
+      tGenes <- lapply(tGenes, strsplit, split = "[", fixed = TRUE)
+      tGenes <- lapply(tGenes, "[[", 1)
+      
+      GeneNames <- unlist(lapply(tGenes, "[[", 1))
+      
+      Weigths <- rep(NA, length(GeneNames))
+      if(any(lapply(tGenes, length) > 1)){
+        Weigths[lapply(tGenes, length) > 1] <- unlist(lapply(tGenes[lapply(tGenes, length) > 1], "[[", 2))
+      }
+      
+      GeneList[[Idx]] <- list(Name = tLine[1], Desc = tLine[2], Genes = GeneNames, Weigths = Weigths)
     }
     
   }
