cc5aad8c472f82f5af96470b46f8c727153a89e3
Luca Albergante <luca.albergante@gmail.com>
2017-04-24 11:11:49 +0200

    Included DashboardA

--- R/dashboards.R
+++ R/dashboards.R
@@ -1,4 +1,14 @@
-DashboardA <- function(RomaData, Groups, ExpMat) {
+#' Title
+#'
+#' @param RomaData 
+#' @param Groups 
+#' @param ExpMat 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+DashboardA <- function(RomaData, Groups, ExpMat, Interactive = TRUE) {
   
   #
   # This is a Shiny web application. You can run the application by clicking
@@ -10,26 +20,46 @@ DashboardA <- function(RomaData, Groups, ExpMat) {
   #
   
   library(shiny)
-  
+  if(Interactive){
+    library(plotly)
+  } else {
+    if(R.utils::isPackageLoaded("plotly")){
+      detach("package:plotly", unload=TRUE)
+    }
+  }
+ 
   ProcessedSamples <- colnames(RomaData$ProjMatrix)
+  
+  if(is.null(Groups)){
+    Groups <- rep("N/A", length(ProcessedSamples))
+    names(Groups) <- ProcessedSamples
+  }
   
   GSList <- as.list(1:nrow(RomaData$PVVectMat))
   names(GSList) <- as.list(rownames(RomaData$ProjMatrix))
   GSList <- GSList[order(names(GSList))]
   
-  Proj <- irlba::prcomp_irlba(t(ExpMat[,ProcessedSamples]), 2, retx = TRUE)$x 
-  rownames(Proj) <- ProcessedSamples
-  colnames(Proj)
+  PCAProj <- irlba::prcomp_irlba(t(ExpMat[,ProcessedSamples]), 2, retx = TRUE)$x 
+  rownames(PCAProj) <- ProcessedSamples
+  colnames(PCAProj) <- c("PC1", "PC2")
+  
+  # Projs <- PCAProj
   
   # Define UI for application that draws a histogram
   ui <- pageWithSidebar(
     
     # Application title
-    headerPanel("rRoma dashboard A"),
+    headerPanel("rRoma / Sample dashboard"),
     
     # Sidebar with a slider input for number of bins
     
     sidebarPanel(
+      selectInput("prjt", "Projectin type:",
+                  list("PCA" = "PCA", "tSNE" = "tSNE")),
+      sliderInput("perp", "tSNE perplexity:",
+                  min = 0,  max = 50,  value = 0, step = .1),
+      hr(),
+      
       selectInput("disp", "Dispersion filter:",
                   list("Overdispersed" = "Over", "Underdispersed" = "Under", "None" = "None")),
       sliderInput("pdisp", "Log 10 p-value threshold:",
@@ -48,7 +78,12 @@ DashboardA <- function(RomaData, Groups, ExpMat) {
     ),
     
     mainPanel(
-      plotly::plotlyOutput("scatPlot")
+      if(Interactive){
+        plotlyOutput("scatPlot")
+      } else {
+        plotOutput("scatPlot")
+      }
+      
     )
     
   )
@@ -60,41 +95,35 @@ DashboardA <- function(RomaData, Groups, ExpMat) {
       as.integer(input$gs)
     })
     
+    GetProj <- reactive({
+      
+      if(input$prjt == "PCA"){
+        PCAProj
+      }
+      
+      if(input$prjt == "tSNE"){
+        tSNEProj <- Rtsne::Rtsne(X = t(ExpMat[,ProcessedSamples]), perplexity = input$perp)$Y
+        rownames(tSNEProj) <- ProcessedSamples
+        colnames(tSNEProj) <- c("PC1", "PC2")
+        
+        tSNEProj
+      }
+    })
     
     observe({
-      
-      # print(input$disp)
-      # print(input$exp)
-      
       
       Idx <- SelectGeneSets(RomaData = RomaData, VarThr = 10^input$pdisp, VarMode = "Wil",
                             VarType = input$disp,
                             MedThr = 10^input$pexp, MedMode = "Wil",
                             MedType = input$exp)
       
-      # print(Idx)
-      
-      # SelectGeneSets(RomaData = RomaData, VarThr = 1, VarMode = "Wil",
-      #                VarType = "Over",
-      #                MedThr = 1, MedMode = "Wil",
-      #                MedType = "input$exp")
-      
       GSList <- Idx
       names(GSList) <- as.list(rownames(RomaData$ProjMatrix)[Idx])
       GSList <- GSList[order(names(GSList))]
       
-      updateSelectInput(session, "gs", choices = GSList, selected = Idx[1])
-      
-      # choice1<-c("NONE",setdiff(heirarchy,c(hei2,hei3)))
-      # choice2<-c("NONE",setdiff(heirarchy,c(hei1,hei3)))
-      # choice3<-c("NONE",setdiff(heirarchy,c(hei1,hei2)))
-      # 
-      # updateSelectInput(session,"heir1",choices=choice1,selected=hei1)
-      # updateSelectInput(session,"heir2",choices=choice2,selected=hei2)
-      # updateSelectInput(session,"heir3",choices=choice3,selected=hei3)
+      updateSelectInput(session, "gs", choices = GSList, selected = GSList[[1]])
       
     })
-    
     
     output$info <- renderUI({
       str1 <- paste("Overdispersed PV = ", signif(RomaData$PVVectMat[SelectedGS(), 1]))
@@ -105,20 +134,50 @@ DashboardA <- function(RomaData, Groups, ExpMat) {
       
     })
     
-    output$scatPlot <- plotly::renderPlotly({
-      
-      p <- ggplot2::ggplot(data = data.frame(Comp1 = Proj[,"PC1"],
-                                             Comp2 = Proj[,"PC2"],
-                                             Score = RomaData$ProjMatrix[SelectedGS(), ProcessedSamples],
-                                             Group = Groups[ProcessedSamples]),
-                           ggplot2::aes(x = Comp1, y = Comp2, shape = Group, color = Score)) +
-        ggplot2::geom_point(ggplot2::aes(text = rownames(Proj))) +
-        ggplot2::scale_color_gradient2(low = "blue", high = "red", mid = "white") +
-        ggplot2::labs(x = "Component 1", y = "Component 2", shape = "")
-      
-      print(plotly::ggplotly(p))
-      # print(p)
-    })
+    if(Interactive){
+      output$scatPlot <- renderPlotly({
+        
+        Projs <- GetProj()
+        
+        if(is.null(Projs)){
+          Projs <- PCAProj
+        }
+        
+        p <- ggplot2::ggplot(data = data.frame(Comp1 = Projs[,"PC1"],
+                                               Comp2 = Projs[,"PC2"],
+                                               Score = RomaData$ProjMatrix[SelectedGS(), ProcessedSamples],
+                                               Group = Groups[ProcessedSamples]),
+                             ggplot2::aes(x = Comp1, y = Comp2, shape = Group, color = Score)) +
+          ggplot2::geom_point(ggplot2::aes(text = rownames(Projs))) +
+          ggplot2::scale_color_gradient2(low = "blue", high = "red", mid = "white") +
+          ggplot2::labs(x = "Component 1", y = "Component 2", shape = "",
+                        title = rownames(RomaData$ProjMatrix)[SelectedGS()])
+        
+        print(ggplotly(p))
+      })
+    } else {
+      output$scatPlot <- renderPlot({
+        
+        Projs <- GetProj()
+        
+        if(is.null(Projs)){
+          Projs <- PCAProj
+        }
+        
+        p <- ggplot2::ggplot(data = data.frame(Comp1 = Projs[,"PC1"],
+                                               Comp2 = Projs[,"PC2"],
+                                               Score = RomaData$ProjMatrix[SelectedGS(), ProcessedSamples],
+                                               Group = Groups[ProcessedSamples]),
+                             ggplot2::aes(x = Comp1, y = Comp2, shape = Group, color = Score)) +
+          ggplot2::geom_point(ggplot2::aes(text = rownames(Projs)), size = 3) +
+          ggplot2::scale_color_gradient2(low = "blue", high = "red", mid = "white") +
+          ggplot2::labs(x = "Component 1", y = "Component 2", shape = "",
+                        title = rownames(RomaData$ProjMatrix)[SelectedGS()])
+        
+        print(p)
+      })
+    }
+    
   }
   
   # Run the application 
