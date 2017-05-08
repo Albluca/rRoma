d2127f68911da90d798b5910357f3b3a6101082b
Luca Albergante <luca.albergante@gmail.com>
2017-05-08 20:55:14 +0200

    added function get correlations

--- R/correlations.R
+++ R/correlations.R
@@ -1,50 +1,75 @@
-GetCorrelations <- function(RomaData, Selected = NULL, MatData, Methods = "pearson", ConfLevel = .95) {
-  
-  if(is.null()){
+#' Title
+#'
+#' @param RomaData
+#' @param Selected
+#' @param MatData
+#' @param Methods
+#' @param ConfLevel
+#'
+#' @return
+#' @export
+#'
+#' @examples
+GetCorrelations <- function(RomaData, Selected = NULL, MatData, Methods = "pearson",
+                            ConfLevel = .95, Construct.Net = FALSE, Construct.Net.PVThr = 1e-2) {
+
+  if(is.null(Selected)){
     Selected <- 1:length(RomaData$ModuleSummary)
   }
-  
+
+  GeneNets <- NULL
+
   AllCor <- NULL
-  
+
   SampleIds <- intersect(colnames(MatData), colnames(RomaData$ProjMatrix))
-  
+
   for(i in Selected){
-    
+
     CorVal <- apply(MatData[RomaData$ModuleSummary[[i]]$UsedGenes, SampleIds], 1, function(x){
       CT <- cor.test(x = x, y = RomaData$ProjMatrix[i, SampleIds], conf.level = ConfLevel, method = Methods)
       c(CT$estimate, CT$conf.int, CT$p.value)
     })
-    
+
     CorVal <- rbind(colnames(CorVal), CorVal, rep(rownames(RomaData$ProjMatrix)[i], ncol(CorVal)))
-    rownames(CorVal) <- c("gene", "cor", "ci.low", "ci.high", "p.val", "GS")
-    
+
+    if(Methods == "pearson"){
+      rownames(CorVal) <- c("gene", "cor", "ci.low", "ci.high", "p.val", "GS")
+    } else {
+      rownames(CorVal) <- c("gene", "cor", "p.val", "GS")
+    }
+
     AllCor <- cbind(AllCor, CorVal)
-    
+
   }
-  
-  rownames(AllCor) <- NULL
-  
+
   AllCorGenes = t(AllCor)
-  
-  
+
+
   AllCor <- NULL
-  
+
   for(i in Selected){
-    
+
     CorVal <- apply(MatData[RomaData$ModuleSummary[[i]]$UsedGenes, SampleIds], 2, function(x){
       CT <- cor.test(x = x, y = (RomaData$WeigthList[[i]])[names(x)], conf.level = ConfLevel, method = Methods)
       c(CT$estimate, CT$conf.int, CT$p.value)
     })
-    
+
     CorVal <- rbind(colnames(CorVal), CorVal, rep(rownames(RomaData$ProjMatrix)[i], ncol(CorVal)))
-    rownames(CorVal) <- c("sample", "cor", "ci.low", "ci.high", "p.val", "GS")
-    
+
+    if(Methods == "pearson"){
+      rownames(CorVal) <- c("sample", "cor", "ci.low", "ci.high", "p.val", "GS")
+    } else {
+      rownames(CorVal) <- c("sample", "cor", "p.val", "GS")
+    }
+
+
+
     AllCor <- cbind(AllCor, CorVal)
-    
+
   }
-  
+
   AllCorSamples = t(AllCor)
-  
-  return(Genes = AllCorGenes, Samples = AllCorSamples)
-  
+
+  return(list(Genes = AllCorGenes, Samples = AllCorSamples))
+
 }
